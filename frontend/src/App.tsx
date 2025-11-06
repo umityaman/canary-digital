424917a08c9d7311dad012a1fe328031160370a5
CANARY Team <team@canary-erp.com>
2025-11-06 21:52:37 +0300

    refactor: remove Bank and E-commerce pages from sidebar (now tabs in Accounting)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,8 +39,6 @@ const SEOMarketing = lazy(() => import('./pages/website/SEOMarketing'))
 const FinanceReports = lazy(() => import('./pages/website/FinanceReports'))
 const SecuritySettings = lazy(() => import('./pages/website/SecuritySettings'))
 const Todo = lazy(() => import('./pages/Todo'))
-const BankIntegrations = lazy(() => import('./components/banking/BankIntegrations'))
-const ECommerceIntegrations = lazy(() => import('./components/ecommerce/ECommerceIntegrations'))
 const Messaging = lazy(() => import('./pages/Messaging'))
 const Meetings = lazy(() => import('./pages/Meetings'))
 const Tools = lazy(() => import('./pages/Tools'))
@@ -113,8 +111,6 @@ export default function App(){
               <Route path='/accounting/quote/:id' element={<QuoteDetail />} />
               <Route path='/account-cards' element={<AccountCardList />} />
               <Route path='/account-cards/:id' element={<AccountCardDetail />} />
-              <Route path='/bank-integrations' element={<BankIntegrations />} />
-              <Route path='/ecommerce-integrations' element={<ECommerceIntegrations />} />
               <Route path='/social' element={<Social />} />
               <Route path='/website' element={<Website />} />
               <Route path='/website/builder' element={<SiteBuilder />} />
